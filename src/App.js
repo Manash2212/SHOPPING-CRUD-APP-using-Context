df9273c9a54913ebe4afd1fb3a0937169e770c18
Manash <manashhalder2212@gmail.com>
2024-05-31 21:56:45 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>This is Shopping App</h1>
+      <h1 className="text-xl font-bold">This is Shopping App</h1>
     </div>
   );
 }
